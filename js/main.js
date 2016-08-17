b7f477a7c31f1661b602b6e64eb9cb5616bcea98
Kon Kim <kon.kim@outlook.com>
2016-08-16 20:22:39 -0400

    initial

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function(){
+
+$('.gallery').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: 4000
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+})();
